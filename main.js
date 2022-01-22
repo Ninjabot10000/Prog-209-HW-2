8f3440cd9a3a382a764fdafeee280142a3d40441
Ninjabot10000 <ninjabot10000@gmail.com>
2022-01-21 18:33:49 -0800

    updated homework

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-//https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-80.php Very useful for swapping letters
-//https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/array/splice
 let fruit = [];
 
 document.getElementById("changeBtn").style.visibility = 'hidden';
 
 function promptUser(){
-    for (let i = 0; i <= 2; i++) {
+    for (let i = 0; i < 3; i++) {
         fruit[i] = prompt("Name a fruit");
     }
     //document.getElementById("buttonDisappear").innerHTML = "";
@@ -13,24 +11,23 @@ function promptUser(){
     if (startBtn.style.display = 'none'){
         document.getElementById("changeBtn").style.visibility = 'visible';
     }
+    fruitdisplay();
  }
-console.log(fruit)
+ 
 
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     // disable the lower part of the form until we have user input for all values
-//     //document.getElementById("experience").disabled = true;
-// })
-
-function fruitdisplay(){
+ function fruitdisplay(){
+    console.log(fruit)
     let ul = document.createElement('ul');
-    document.getElementById('fruitul').appendChild(ul);
-    for (let i = 0; i <= 2; i++) {
+    document.getElementById("fruitul").appendChild(ul);
+    // document.getElementById('fruitul').appendChild(ul);
+    for (i = 0; i < fruit.length; i++) {
         let li = document.createElement('li');
         ul.appendChild(li);
-        li.innerHTML = fruit[i];
-    }
+        // let text = document.createTextNode(fruit[i]);
+        // let text = document.getElementsByName(fruit[i]);
+        li.innerHTML = fruit[i];    
+    };
 }
-fruitdisplay();
 
 changeBtn.addEventListener("click",changeWordDisplay, false)
 
@@ -38,6 +35,46 @@ function changeWordDisplay(){
     firstScreen.style.display = "none";
     secondScreen.style.display = "block";
 }
+
+function letterSwap(){
+    let fruit2 = fruit.map(x => x.charAt(fruit.length - 1) + x.substring(1, x.length -1) + x.charAt(0));
+    let ul = document.createElement('ul');
+    document.getElementById("fruitul2").appendChild(ul);
+    for (i = 0; i < fruit2.length; i++) {
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML = fruit2[i];    
+    };
+    console.log(fruit2)
+}
+//2nd array 
+// document.addEventListener("DOMContentLoaded", function (event) {
+
+
+    // document.getElementById("changeBtn").addEventListener("click", function(){
+    //     let x = fruit.map(fruit => {
+    //         x.charAt(fruit.length - 1) + x.substring(1, x.length -1) + x.charAt(0);
+    //     });
+    // });
+    //     document.getElementById("fruitul2").value = x;
+    //     console.log(x);
+// });
+
+// function letterSwap(){
+//     swap = x.charAt(fruit.length - 1) + x.substring(1, x.length -1) + x.charAt(0);
+//     return swap
+// }
+
+
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     // disable the lower part of the form until we have user input for all values
+//     //document.getElementById("experience").disabled = true;
+// })
+
+
+
+
 
 
 
